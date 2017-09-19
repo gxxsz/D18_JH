a1431657d2cbd7e7602331e29bdd0eb213ce5f22
gxxsz <947gxxsz@163.com>
2017-09-19 15:03:48 +0800

    first

---
+++ D18_Helper/D18_Helper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  D18_Helper
+//
+//  Created by azz on 2017/9/19.
+//  Copyright © 2017年 com.JH.TD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ D18_Helper/D18_Helper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  D18_Helper
+//
+//  Created by azz on 2017/9/19.
+//  Copyright © 2017年 com.JH.TD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
