36031c1b7af149dd0f93627a69df1c2bbee57a31
gxxsz <947gxxsz@163.com>
2017-10-14 17:53:56 +0800

    频率设置

--- D18_Helper/D18_Helper/FMSetting/D18_FMSettingViewController.m
+++ D18_Helper/D18_Helper/FMSetting/D18_FMSettingViewController.m
@@ -7,12 +7,29 @@
 //
 
 #import "D18_FMSettingViewController.h"
+#import "D18_SliderView.h"
 
 @interface D18_FMSettingViewController ()<D18_NavigationBarDelegate>
 
 @property (nonatomic , strong) UIView *leftFMView;
 
 @property (nonatomic , strong) UIView *rightFMView;
+
+@property (nonatomic , strong) UIView *backgroundView;
+
+@property (nonatomic , strong) D18_SliderView *leftHighSliderView;
+
+@property (nonatomic , strong) D18_SliderView *leftLowSliderView;
+
+@property (nonatomic , strong) D18_SliderView *rightHighSliderView;
+
+@property (nonatomic , strong) D18_SliderView *rightLowSliderView;
+
+@property (nonatomic , assign) CGFloat backgroundViewHight;
+
+@property (nonatomic , strong) UIView *bottomView;
+
+@property (nonatomic , strong) UIImageView *buttonImage;
 
 @end
 
@@ -25,10 +42,12 @@
     D18_NavigationBar *barView = [[D18_NavigationBar alloc] initWithTitle:@"FM" isShowBackButton:YES];
     barView.barViewDelegate = self;
     [self.view addSubview:barView];
+    _backgroundViewHight = kScreenWidth > 320 ? 100 : 80;
     
     [self setUpFMBackgroundView];
     [self setUpLeftFMView];
     [self setUpRightFMView];
+    [self setUpBottomView];
     
 }
 
@@ -40,22 +59,156 @@
 
 - (void)setUpFMBackgroundView
 {
+    CGFloat backgroundViewY = kScreenWidth > 375 ? 150 : 100;
+    _backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, backgroundViewY, kScreenWidth, 300)];
+    _backgroundView.backgroundColor = [Utils stringTOColor:@"#171717"];
+    [self.view addSubview:_backgroundView];
+    
+    CGFloat upBackgroundImageViewY = 30;
+    UIImageView *upBackgroundImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, upBackgroundImageViewY, kScreenWidth, _backgroundViewHight)];
+    upBackgroundImageView.image = [UIImage imageNamed:@"FMBackground_up"];
+    [_backgroundView addSubview:upBackgroundImageView];
+
+    CGFloat middleBackgroundImageViewY = CGRectGetMaxY(upBackgroundImageView.frame)+ 30;
+    UIImageView *middleBackgroundImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, middleBackgroundImageViewY, kScreenWidth, _backgroundViewHight-20)];
+    middleBackgroundImageView.image = [UIImage imageNamed:@"FMBackground_Middle"];
+    [_backgroundView addSubview:middleBackgroundImageView];
+
+    CGFloat downBackgroundImageViewY = CGRectGetMaxY(middleBackgroundImageView.frame)+ 30;
+    UIImageView *downBackgroundImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, downBackgroundImageViewY, kScreenWidth, _backgroundViewHight)];
+    downBackgroundImageView.image = [UIImage imageNamed:@"FMBackground_down"];
+    [_backgroundView addSubview:downBackgroundImageView];
+    _backgroundView.height = CGRectGetMaxY(downBackgroundImageView.frame)+30;
+    
+    [self frequencyViewWithYvalue:0 isHighFM:YES];
+    CGFloat bottomFequencyViewY = _backgroundView.height - 30;
+    [self frequencyViewWithYvalue:bottomFequencyViewY isHighFM:NO];
     
 }
 
 - (void)setUpLeftFMView
 {
+    _leftFMView = [[UIView alloc] initWithFrame:_backgroundView.bounds];
+    _leftFMView.backgroundColor = [UIColor clearColor];
+
+
+    CGFloat leftHighY = _backgroundViewHight-30;
+    _leftHighSliderView = [[D18_SliderView alloc] initWithFrame:CGRectMake(0, leftHighY, kScreenWidth, _backgroundViewHight) showValue:YES valueIsAbove:YES];
+    [_leftFMView addSubview:_leftHighSliderView];
+
+    CGFloat leftLowY = CGRectGetMaxY(_leftHighSliderView.frame) + _backgroundViewHight-(kScreenWidth > 320 ? 30 : 10);
+    _leftLowSliderView = [[D18_SliderView alloc] initWithFrame:CGRectMake(0, leftLowY, kScreenWidth, _backgroundViewHight) showValue:YES valueIsAbove:NO];
+    [_leftFMView addSubview:_leftLowSliderView];
+    [_backgroundView bringSubviewToFront:_leftFMView];
     
+    [_backgroundView addSubview:_leftFMView];
 }
 
 - (void)setUpRightFMView
 {
     
+    _rightFMView = [[UIView alloc] initWithFrame:_backgroundView.bounds];
+    _rightFMView.backgroundColor = [UIColor clearColor];
+    [_backgroundView addSubview:_rightFMView];
+
+
+    CGFloat rightHighY = _backgroundViewHight-30;
+    _rightHighSliderView = [[D18_SliderView alloc] initWithFrame:CGRectMake(0, rightHighY, kScreenWidth, _backgroundViewHight) showValue:YES valueIsAbove:YES];
+    [_rightFMView addSubview:_rightHighSliderView];
+
+    CGFloat rightLowY = CGRectGetMaxY(_rightHighSliderView.frame) + _backgroundViewHight-(kScreenWidth > 320 ? 30 : 10);
+    _rightLowSliderView = [[D18_SliderView alloc] initWithFrame:CGRectMake(0, rightLowY, kScreenWidth, _backgroundViewHight) showValue:YES valueIsAbove:NO];
+    [_rightFMView addSubview:_rightLowSliderView];
+    _rightFMView.hidden = YES;
+    
+    
 }
+
+- (void)setUpBottomView
+{
+    CGFloat bottomViewY = CGRectGetMaxY(_backgroundView.frame) + 50;
+    _bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, bottomViewY, kScreenWidth, 50)];
+    _bottomView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:_bottomView];
+    
+    _buttonImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 140, 50)];
+    _buttonImage.image = [UIImage imageNamed:@"FMChange_Left"];
+    [_bottomView addSubview:_buttonImage];
+    _buttonImage.centerX = kScreenWidth*0.5;
+    _buttonImage.contentMode = UIViewContentModeCenter;
+    
+    CGFloat leftButtonX = kScreenWidth * 0.5 - 70;
+    UIButton *leftButton = [[UIButton alloc] initWithFrame:CGRectMake(leftButtonX, 0, 70, 50)];
+    [leftButton addTarget:self action:@selector(changeToLeftFMView) forControlEvents:UIControlEventTouchUpInside];
+    leftButton.backgroundColor = [UIColor clearColor];
+    [_bottomView addSubview:leftButton];
+    
+    
+    CGFloat rightButtonX = kScreenWidth * 0.5;
+    UIButton *rightButton = [[UIButton alloc] initWithFrame:CGRectMake(rightButtonX, 0, 70, 50)];
+    [rightButton addTarget:self action:@selector(changeToRightFMView) forControlEvents:UIControlEventTouchUpInside];
+    rightButton.backgroundColor = [UIColor clearColor];
+    [_bottomView addSubview:rightButton];
+}
+
+- (void)frequencyViewWithYvalue:(CGFloat)Yvalue isHighFM:(BOOL)isHigh
+{
+    UIView *frequencyView = [[UIView alloc] initWithFrame:CGRectMake(0, Yvalue, kScreenWidth, 30)];
+    frequencyView.backgroundColor = [UIColor clearColor];
+    [_backgroundView addSubview:frequencyView];
+    
+    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 8, kScreenWidth, 15)];
+    label1.text = isHigh ? @"High Frequency" : @"Low Frequency";
+    label1.textColor = [Utils stringTOColor:@"#999999"];
+    label1.font = [UIFont systemFontOfSize:12];
+    [label1 sizeToFit];
+    label1.center =  CGPointMake(kScreenWidth*0.5, 30*0.5);
+    label1.backgroundColor = [UIColor clearColor];
+    [frequencyView addSubview:label1];
+    
+    CGFloat frontViewX = CGRectGetMinX(label1.frame) - 28;
+    UIView *frontView = [[UIView alloc] initWithFrame:CGRectMake(frontViewX, 10, 20, 0.5)];
+    frontView.backgroundColor = [Utils stringTOColor:@"#999999"];
+    frontView.centerY = label1.centerY;
+    [frequencyView addSubview:frontView];
+    
+    CGFloat behindViewX = CGRectGetMaxX(label1.frame) + 8;
+    UIView *behindView = [[UIView alloc] initWithFrame:CGRectMake(behindViewX, 10, 20, 0.5)];
+    behindView.backgroundColor = [Utils stringTOColor:@"#999999"];
+    behindView.centerY = label1.centerY;
+    [frequencyView addSubview:behindView];
+    
+}
+
 
 - (void)backBtnClick
 {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)changeToLeftFMView
+{
+    [self showRightFMView:NO];
+}
+
+- (void)changeToRightFMView
+{
+    [self showRightFMView:YES];
+}
+
+- (void)showRightFMView:(BOOL)showRight
+{
+    if (showRight) {
+        _buttonImage.image = [UIImage imageNamed:@"FMChange_Right"];
+        _leftFMView.hidden = YES;
+        _rightFMView.hidden = NO;
+        
+    }else{
+        _buttonImage.image = [UIImage imageNamed:@"FMChange_Left"];
+        _leftFMView.hidden = NO;
+        _rightFMView.hidden = YES;
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
--- D18_Helper/D18_Helper/FMSetting/views/D18_SliderView.m
+++ D18_Helper/D18_Helper/FMSetting/views/D18_SliderView.m
@@ -17,6 +17,10 @@
 
 @property (nonatomic , strong) UISlider *slider;
 
+@property (strong, nonatomic) CAGradientLayer *gradientLayer;
+
+@property (nonatomic , assign) CGFloat valueX;
+
 @end
 
 @implementation D18_SliderView
@@ -28,7 +32,9 @@
     if (self) {
         _isShowValue = show;
         _isAbove = isAbove;
-        _sliderHeight = 30;
+        _sliderHeight = 10;
+        self.backgroundColor = [UIColor clearColor];
+        _valueX = 0.0;
         [self setUpUI];
     }
     return self;
@@ -36,33 +42,74 @@
 
 - (void)setUpUI
 {
-    if (_isAbove) {
-        _valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 10, 80, 80)];
-        [self addSubview:_valueLabel];
-        
-        CGFloat sliderY = CGRectGetMaxY(_valueLabel.frame) + 15;
-        _slider = [[UISlider alloc] initWithFrame:CGRectMake(20, sliderY, kScreenWidth - 40, _sliderHeight)];
-        [self addSubview:_slider];
-    }else{
-        CGFloat sliderY = CGRectGetMaxY(_valueLabel.frame) + 15;
-        _slider = [[UISlider alloc] initWithFrame:CGRectMake(20, 0, kScreenWidth - 40, _sliderHeight)];
-        [self addSubview:_slider];
-        
-        _valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 10, 80, 80)];
-        [self addSubview:_valueLabel];
-        
-        
-    }
+    CGFloat sliderY = _isAbove ? 60 : 0;
+    UIImageView *sliderBackgroundView = [[UIImageView alloc] initWithFrame:CGRectMake(0, sliderY, kScreenWidth, 20)];
+    UIImage *backgroundImage = [UIImage imageNamed:@"FMBar"];
+    sliderBackgroundView.image = backgroundImage;
+    sliderBackgroundView.height = backgroundImage.size.height;
+    [self addSubview:sliderBackgroundView];
     
+    _slider = [[UISlider alloc] initWithFrame:CGRectMake(20, sliderY, kScreenWidth - 40, _sliderHeight)];
+    _slider.centerY = sliderBackgroundView.centerY;
     
+    [self addSubview:_slider];
+    
+    CGFloat labelX = 25;
+    CGFloat labelW = 58;
+    CGFloat labelY =  _isAbove ? 0 : CGRectGetMaxY(_slider.frame) + 10;
+    _valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(labelX, labelY, labelW, labelW)];
+    
+    [self addSubview:_valueLabel];
     _valueLabel.text = @"0";
     _valueLabel.textColor = [Utils stringTOColor:@"#ffffff"];
     _valueLabel.font = [UIFont systemFontOfSize:58];
-    _valueLabel.textAlignment = NSTextAlignmentCenter;
     
     [_slider setThumbImage:[UIImage imageNamed:@"slide_HighLighted"] forState:UIControlStateNormal];
-    _slider.minimumTrackTintColor = [Utils stringTOColor:@"#22a1fe"];
-    _slider.maximumTrackTintColor = [Utils stringTOColor:@"#3ae0fe"];
+    _slider.backgroundColor = [UIColor clearColor];
+    _slider.minimumTrackTintColor = [UIColor clearColor];
+    _slider.maximumTrackTintColor = [UIColor clearColor];
+    [_slider addTarget:self action:@selector(changeValueLabelText:) forControlEvents:UIControlEventValueChanged];
+    _slider.minimumValue = 0.0;
+    _slider.maximumValue = 4.0;
+    
+    CGRect layerFrame = CGRectMake(_slider.x, 0, 0, _slider.height);
+    _gradientLayer =  [CAGradientLayer layer];
+    _gradientLayer.frame = layerFrame;
+    [_slider.layer addSublayer:_gradientLayer];
+    _gradientLayer.cornerRadius = 5;
+    
+    
+    //设置渐变颜色方向
+    self.gradientLayer.startPoint = CGPointMake(0, 0);
+    self.gradientLayer.endPoint = CGPointMake(1, 0);
+    
+    //设定颜色组
+    self.gradientLayer.colors = @[(__bridge id)[Utils stringTOColor:@"#22a1fe"].CGColor,
+                                  (__bridge id)[Utils stringTOColor:@"#3ae0fe"].CGColor];
+    
+    //设定颜色分割点
+    self.gradientLayer.locations = @[@(0.5f) ,@(1.0f)];
+}
+
+- (void)changeValueLabelText:(UISlider *)slider
+{
+    CGFloat value = slider.value;
+    _valueX = value;
+    
+    NSString *text = [NSString stringWithFormat:@"%ld",(NSInteger)value];
+    _valueLabel.text = text;
+    CGFloat labelX = 24 + value*(_slider.width-40)/4.0;
+
+     _valueLabel.x = labelX;
+    CGRect frame = _gradientLayer.frame;
+    frame.size.width = labelX-10;
+    if (labelX == 30.0) {
+        frame.size.width = 0;
+    }
+    if (labelX > _slider.width-40) {
+        frame.size.width =  _slider.width-40;
+    }
+    _gradientLayer.frame = frame;
 }
 
 /*
